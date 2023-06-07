5e47ac21ae0f08f7cb7a6d6bc20510c12df7cc97
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-07 09:54:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-// your code here
 function updateURL() {
       var name = document.getElementById("name").value;
       var year = document.getElementById("year").value;
 
-      var urlString = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+      var urlString = "https://localhost:8080/";
+      
+      if (name) {
+        urlString += "?name=" + encodeURIComponent(name);
+      }
+      
+      if (year) {
+        urlString += (name ? "&" : "?") + "year=" + encodeURIComponent(year);
+      }
 
       document.getElementById("url").textContent = urlString;
-}
+    }
