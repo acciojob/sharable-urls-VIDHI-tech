38e98524d9ffcdd2983f0d15a29dc631c79f9fa4
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-07 09:45:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // your code here
+function updateURL() {
+      var name = document.getElementById("name").value;
+      var year = document.getElementById("year").value;
+
+      var urlString = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+
+      document.getElementById("url").textContent = urlString;
+}
